4606c8966dec6600428d943f84ef33349e52add3
Ednaldo Lima <edinlima@gmail.com>
2023-01-03 18:01:51 -0300

    finalização do back-end

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,21 @@
 const express = require('express');
 
-const OngController = require('./controllers/ongController');
+const ongController = require('./controllers/ongController');
+const incidentController = require('./controllers/incidentController');
+const profileController = require('./controllers/profileController');
+const sessionController = require('./controllers/sessionController');
 
 
 const routes = express.Router();
 
-routes.get('/ongs', OngController.index );
-routes.post('/ongs', OngController.create);
+routes.post('/sessions', sessionController.create)
+routes.get('/incidents', incidentController.index );
+routes.post('/incidents', incidentController.create );
+routes.delete('/incidents/:id', incidentController.delete );
+routes.get('/profile', profileController.index );
+
+routes.get('/ongs', ongController.index );
+routes.post('/ongs', ongController.create);
 
 
 module.exports = routes;
